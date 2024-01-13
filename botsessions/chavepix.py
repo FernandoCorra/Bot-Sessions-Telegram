6a64626c0801fcac45908afb322be478a576e8f2
luigirovani <63375368+luigirovani@users.noreply.github.com>
2024-01-13 13:52:05 -0300

    Update chavepix.py
    fix problens in recursive calls

--- botsessions/chavepix.py
+++ botsessions/chavepix.py
@@ -5,6 +5,7 @@ from verificar import get_credenciais
 APIMERCADO = get_credenciais()['mercadopago_api']
 credentials = {'access_token' : APIMERCADO  }
 
+
 def get_payment(price, description):
     sdk = mercadopago.SDK(APIMERCADO)
     payment_data = {
@@ -12,38 +13,40 @@ def get_payment(price, description):
         "description": str(description),
         "payment_method_id": "pix",
         "payer": {
-            "email": "thayanello@hotmail.com",
-            "first_name": "Leticia",
-            "last_name": "Lopes",
+            "email": "luigi3520@gmail.com",
+            "first_name": "Luigi",
+            "last_name": "Rovani",
             "identification": {
                 "type": "CPF",
-                "number": "36473976876"
+                "number": "09018841978"
             },
             "address": {
                 "zip_code": "12576-624",
                 "street_name": "Rua Itabaiana",
                 "street_number": "1",
-                "neighborhood": "Itaguaçu",
+                "neighborhood": "ItaguaÃ§u",
                 "city": "Aparecida",
                 "federal_unit": "SP"
             }
         }
     }
+    
     payment_response = sdk.payment().create(payment_data)
-    payment = payment_response["response"]
-    data = payment['point_of_interaction']['transaction_data']
-    return [str(data['qr_code']),str(payment['id'])]
+    if payment_response['status'] == 201:
+        payment = payment_response["response"]
+        data = payment['point_of_interaction']['transaction_data']
+        return [str(data['qr_code']),str(payment['id'])]
+    else:
+        return None, payment_response['response']['message']
 
 
 async def verify_payment(payment_id):
     sdk = mercadopago.SDK(APIMERCADO)
-    async def get_status():
-        await asyncio.sleep(15)
-        payment_response = await asyncio.to_thread(sdk.payment().get, int(payment_id))
-        payment = payment_response["response"]
-        if payment['status'] == "accredited" and payment['status_detail'] == "approved":
-            return True
-        else:
-            await get_status()
-    
-    return await get_status()
+    await asyncio.sleep(15)
+    payment_response = await asyncio.to_thread(sdk.payment().get, int(payment_id))
+    payment = payment_response["response"]
+    if payment['status'] == "approved" and payment['status_detail'] == "accredited":
+        return True
+    else:
+        return await verify_payment(payment_id)
+        
